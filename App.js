31a3372efdcc4b41f7778b627b6164bcfa20c8e8
Eddie Kanevsky <Eddiekanevsky@gmail.com>
2023-04-14 15:13:43 +0300

    Started working on camera integration

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Chatbot from 'react-chatbot-kit'
-import 'react-chatbot-kit/build/main.css'
+// import 'react-chatbot-kit/build/main.css'
 import config from './bot/config'
 import MessageParser from './bot/MessageParser'
 import ActionProvider from './bot/ActionProvider'
-
+import ShareCamera from './components/ShareCamera/ShareCamera.jsx'
 export default function App() {
 
   return (
-    <div className='App'>
-        <Chatbot
-          config={config}
-          messageParser={MessageParser}
-          actionProvider={ActionProvider}
-        />
-    </div>
+    <ShareCamera />
+    // <div className='App'>
+    //     {/* <Chatbot
+    //       config={config}
+    //       messageParser={MessageParser}
+    //       actionProvider={ActionProvider}
+    //     /> */}
+    // </div>
   );
 }
 
