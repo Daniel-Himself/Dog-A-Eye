cfb38c74b2658a8f616ea3c754010d1a7d781a9f
Dror Mor <mor.dror131@gmail.com>
2023-04-14 18:06:33 +0300

    Fixed the chatbot responsive view

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, Dimensions } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
-import Chatbot from 'react-chatbot-kit'
-import 'react-chatbot-kit/build/main.css'
-import config from './bot/config'
-import MessageParser from './bot/MessageParser'
-import ActionProvider from './bot/ActionProvider'
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+import config from './bot/config';
+import MessageParser from './bot/MessageParser';
+import ActionProvider from './bot/ActionProvider';
+
 
 export default function App() {
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
 
   return (
-    <div className='App'>
+    <View style={[styles.container, {width: windowWidth, height: windowHeight}]}>
         <Chatbot
           config={config}
           messageParser={MessageParser}
           actionProvider={ActionProvider}
         />
-    </div>
+    </View>
   );
 }
 
