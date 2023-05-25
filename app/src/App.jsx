d7a9ccde88716abafa0ae8a458bf521d013d72a2
Dror Mor <mor.dror131@gmail.com>
2023-05-25 10:18:38 +0300

    added UploadIamage Component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { } from "react";
-import Camera from './components/camera'
+// import Camera from './components/camera'
+import UploadImageComponent from "./components/UploadImage";
 import "./style/App.css";
 
 const App = () => {
 
   return (
     <div className="App">
-      <Camera/>
+      <UploadImageComponent/>
     </div>
   );
 };
---
+++ app/src/components/UploadImage.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useRef } from "react";
+
+const UploadImageComponent = () => {
+  const [imageObject, setImageObject] = useState(null);
+
+  const handleFileInput = useRef(null);
+
+  const handleClick = () => {
+    handleFileInput.current.click();
+  };
+
+  const handleImageChange = (event) => {
+    setImageObject({
+      imagePreview: URL.createObjectURL(event.target.files[0]),
+      imageFile: event.target.files[0],
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Upload Photo</button>
+      <label>
+        <input
+          style={{ display: "none" }}
+          type="file"
+          accept="image/*"
+          capture="environment"
+          ref={handleFileInput}
+          onChange={handleImageChange}
+        />
+      </label>
+      {imageObject && <img src={imageObject.imagePreview} />}
+    </div>
+  );
+};
+
+export default UploadImageComponent;
