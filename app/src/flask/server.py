50ca9e9d568c3f5ca8509d85be493d67e6b858d8
Calibourne <Eddiekanevsky@gmail.com>
2023-06-02 20:04:49 +0000

    added a flask server for demonstrating the image enhancement

---
+++ app/src/flask/server.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+import torch
+import torch.nn as nn
+import torchvision
+import torch.backends.cudnn as cudnn
+import torch.optim
+import os
+import sys
+import argparse
+import time
+import dataloader
+import model
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+import glob
+import time
+
+ 
+def lowlight(image_path, save_path):
+    os.environ['CUDA_VISIBLE_DEVICES']='0'
+    torch.cuda.empty_cache()
+    data_lowlight = Image.open(image_path)
+    data_lowlight = (np.asarray(data_lowlight)/255.0)
+    
+    data_lowlight = torch.from_numpy(data_lowlight).float()
+    data_lowlight = data_lowlight.permute(2,0,1)
+    data_lowlight = data_lowlight.cuda().unsqueeze(0)
+    
+    DCE_net = model.enhance_net_nopool().cuda()
+    DCE_net.load_state_dict(torch.load('snapshots/Epoch99.pth'))
+    start = time.time()
+    _,enhanced_image,_ = DCE_net(data_lowlight)
+    
+    end_time = (time.time() - start)
+    print(end_time)
+    result_path = image_path
+    if not os.path.exists(image_path.replace('/'+image_path.split("/")[-1],'')):
+        os.makedirs(image_path.replace('/'+image_path.split("/")[-1],''))
+    torchvision.utils.save_image(enhanced_image, save_path)
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part in the form'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        image = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], "lowlight.jpg"))
+        lowlight(os.path.join(app.config['UPLOAD_FOLDER'], "lowlight.jpg"), os.path.join(app.config['UPLOAD_FOLDER'], "adjusted.jpg"))
+        return render_template('index.html', lowlight_filename="lowlight.jpg", adjusted_filename='adjusted.jpg')
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
