5a098b31ee756acc9b66bc368a97837d0c01f912
AliGranett <89648725+AliGranett@users.noreply.github.com>
2023-12-02 17:04:57 +0200

    added carousel

--- src/components/instructions.jsx
+++ src/components/instructions.jsx
@@ -3,42 +3,50 @@ import "../style/model.css";
 // import Carousel from 'react-bootstrap/Carousel';
 // import { Carousel } from 'react-responsive-carousel';
 
+// Carousel try
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from 'react-bootstrap';
+
+
 
 const Instructions = () => {
-
   return (
-    <div className='container'>
-      <h2 className='title'>Instructions</h2>
-      <p className='text'>
-        Here are some guidelines on how to take a qualitative dog eye picture
-      </p>
-      <ol className='list'>
-        <li className='list-item'>
-          <p>Make sure the dog is calm and comfortable.</p>
-          <div className='instructions-container'>
-            <img className='image-instructions' src={`${process.env.PUBLIC_URL}/comfrtable_d.png`} alt='dog' />
+    <div className="container">
+      <h2 className="title">Instructions</h2>
+      <Carousel className='carusel_style'>
+        <Carousel.Item>
+        <div className="carousel-item-wrapper">
+            <img
+              className="d-block w-100 image-instructions"
+              src={`${process.env.PUBLIC_URL}/comfrtable_d.png`}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <p>Make sure the dog is calm and comfortable.</p>
+            </Carousel.Caption>
           </div>
-        </li>
-        <li  className='list-item'>
-          <p>Find a well-lit area, preferably with natural light.</p>
-          <div className='instructions-container'>
-            <img className='image-instructions' src={`${process.env.PUBLIC_URL}/well_lit_d.png`} alt='dog' />
-          </div>
-        </li>
-        <li className='list-item'>
-          <p>Avoid using flash.</p>
-          <div className='instructions-container'>
-          <img className='image-instructions' src={`${process.env.PUBLIC_URL}/no_flash_d.png`} alt='dog'/>
-          </div>
-        </li>
-        <li  className='list-item'>
-          <p>Capture the image with steady hands to avoid blurriness.</p>
-          <div className='instructions-container'>
-          <img className='image-instructions' src={`${process.env.PUBLIC_URL}/no_blurry_d.png`} alt='dog'/>
-          </div>
-          
-        </li>
-      </ol>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 image-instructions"
+            src={`${process.env.PUBLIC_URL}/well_lit_d.png`}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <p>Find a well-lit area, preferably with natural light.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 image-instructions"
+            src={`${process.env.PUBLIC_URL}/no_flash_d.png`}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <p>Avoid using flash.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
